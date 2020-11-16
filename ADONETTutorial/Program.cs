f3153d0c00eeb7562e97c1ccc52a594b52f2520a
Maximum Code <maximum2000@hotmail.com>
2020-11-15 18:17:24 -0800

    Add project files.

---
+++ ADONETTutorial/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace ADONETTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WorkingWithSqlConnection.SqlConnectionExample();
+        }
+    }
+}
